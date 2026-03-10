2ac58411842c5ededcf1109a1f98ab20c79722c6
adimac13 <s197932@student.pg.edu.pl>
2026-03-10 18:43:13 +0100

    full dataset preparation

--- signal_generator.py
+++ signal_generator.py
@@ -1,21 +1,23 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-class Dataset:
+class SignalGenerator:
     """
     This class prepares a dataset for training. It generates a linearly increasing sine function with noise and incident
     applied.
     """
-    def __init__(self, freq = 2, time_range = 20, num_of_probes = 10000):
+    def __init__(self, freq = 2, time_range = 20, num_of_probes = 10000, next_H_steps = 20, prev_W_steps = 20):
         self.freq = freq
         self.time_range = time_range
         self.num_of_probes = num_of_probes
         self.where_incident = np.zeros(self.num_of_probes)
         self.signal = None
+        self.H_steps = next_H_steps
+        self.W_steps = prev_W_steps
 
     def setup(self):
         """
-        Setup function. It returns generated signal and labels, where: 0 -> normal, 1 -> incident.
+        Setup function. Function creates signal and labels, where: 0 -> normal, 1 -> incident.
         """
         time = np.linspace(0, self.time_range, self.num_of_probes)
         noise = np.random.normal(0, 0.1, self.num_of_probes) / 10
@@ -33,12 +35,29 @@ class Dataset:
 
             i += len_of_incident # To avoid incident overlapping
 
-        if __name__ == "__main__":
-            plt.plot(time, self.signal)
-            plt.show()
-        return self.signal, self.where_incident
+    def prepare_dataset(self):
+        """
+        Function returns windows (consisting of W steps) and label whether in next H steps incident will occur.
+        """
+        self.setup()
+        xs = []
+        ys = []
+        for i in range(len(self.signal) - self.W_steps - 1):
+            # W step window
+            x = self.signal[i:i + self.W_steps]
+
+            # H step window, after W step window
+            max_y_idx = min(len(self.signal) - 1, i + self.W_steps + self.H_steps)
+            y = (1 if 1 in self.where_incident[i + self.W_steps : max_y_idx + 1] else 0)
+
+            xs.append(x)
+            ys.append(y)
+
+        return np.array(xs), np.array(ys)
 
 if __name__ == "__main__":
-    ds = Dataset()
-    ds.setup()
+    ds = SignalGenerator()
+    x,y = ds.prepare_dataset()
+    print(x.shape, y.shape)
+    print(np.sum(y))
     # pass
