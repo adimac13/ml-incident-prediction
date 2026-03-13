01c6b6d68453217ec227c989d4c26f3e29eb203f
adimac13 <s197932@student.pg.edu.pl>
2026-03-13 14:34:33 +0100

    updated lstm model

--- lstm_model.py
+++ lstm_model.py
@@ -1,5 +1,5 @@
 from pytorch_lightning.loggers import TensorBoardLogger
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader, Subset
 import pytorch_lightning as pl
 from signal_generator import SignalGenerator
 from torch import nn, optim, tensor
@@ -7,7 +7,7 @@ import torch
 import torchmetrics
 
 class IncidentDataset(Dataset):
-    def __init__(self, current_window_size = 20, future_window_size = 20, freq = 2, time_range = 20, num_of_probes = 10000):
+    def __init__(self, current_window_size = 200, future_window_size = 100, freq = 2, time_range = 20, num_of_probes = 10000):
         sg = SignalGenerator(future_window_size, current_window_size, freq, time_range, num_of_probes)
         self.input, self.labels = sg.prepare_dataset()
 
@@ -24,7 +24,10 @@ class IncidentDataModule(pl.LightningDataModule):
 
     def setup(self, stage = None):
         ds = IncidentDataset()
-        self.val_dataset, self.train_dataset = random_split(ds, [len(ds) // 10, len(ds) - len(ds) // 10])
+        self.ds = ds
+        train_ds_len = len(ds) - len(ds) // 10
+        self.train_dataset = Subset(ds, range(0,train_ds_len))
+        self.val_dataset = Subset(ds, range(train_ds_len,len(ds)))
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size = self.batch_size, shuffle = True)
@@ -33,20 +36,36 @@ class IncidentDataModule(pl.LightningDataModule):
         return DataLoader(self.val_dataset, batch_size = self.batch_size, shuffle = False)
 
 class IncidentModel(pl.LightningModule):
-    def __init__(self, hidden_dim = 30):
+    def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(input_size = 1, hidden_size=hidden_dim, num_layers=1, batch_first = True, bidirectional=False)
-        self.fc = nn.Linear(hidden_dim, 1)
-        self.loss = nn.BCEWithLogitsLoss()
-        self.acc = torchmetrics.Accuracy(task='binary')
-        self.precision = torchmetrics.Precision(task='binary')
-        self.recall = torchmetrics.Recall(task='binary')
+        self.lstm = nn.LSTM(input_size = 1, hidden_size=128, num_layers=2, batch_first = True, dropout = 0.2)
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.relu  = nn.ReLU()
+        self.loss = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(2.0))
+
+        self.acc_train = torchmetrics.Accuracy(task='binary')
+        self.precision_train  = torchmetrics.Precision(task='binary')
+        self.recall_train  = torchmetrics.Recall(task='binary')
+        self.auroc_train  = torchmetrics.AUROC(task="binary")
+
+        self.acc_val = torchmetrics.Accuracy(task='binary')
+        self.precision_val = torchmetrics.Precision(task='binary')
+        self.recall_val = torchmetrics.Recall(task='binary')
+        self.auroc_val = torchmetrics.AUROC(task="binary")
+
+
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         lstm_out, _ = self.lstm(x)
         lstm_final = lstm_out[:,-1,:]
-        out = self.fc(lstm_final)
+        out = self.fc1(lstm_final)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
         return out
 
     def configure_optimizers(self):
@@ -59,13 +78,15 @@ class IncidentModel(pl.LightningModule):
         loss = self.loss(outputs, labels)
 
         outputs = self.sigmoid(outputs)
-        acc = self.acc(outputs, labels)
-        precision = self.precision(outputs, labels)
-        recall = self.recall(outputs, labels)
+        acc = self.acc_train(outputs, labels)
+        precision = self.precision_train(outputs, labels)
+        recall = self.recall_train(outputs, labels)
+        auroc = self.auroc_train(outputs, labels)
         self.log('train_loss', loss, on_step = True, on_epoch = True)
         self.log('train_acc', acc, on_step=False, on_epoch=True)
         self.log('train_precision', precision, on_step=False, on_epoch=True)
         self.log('train_recall', recall, on_step=False, on_epoch=True)
+        self.log('train_auroc', auroc, on_step = False, on_epoch = True)
 
         return loss
 
@@ -75,21 +96,31 @@ class IncidentModel(pl.LightningModule):
         loss = self.loss(outputs, labels)
 
         outputs = self.sigmoid(outputs)
-        acc = self.acc(outputs, labels)
-        precision = self.precision(outputs, labels)
-        recall = self.recall(outputs, labels)
+        acc = self.acc_val(outputs, labels)
+        precision = self.precision_val(outputs, labels)
+        recall = self.recall_val(outputs, labels)
+        auroc = self.auroc_val(outputs, labels)
         self.log('val_loss', loss, on_step = True, on_epoch = True)
         self.log('val_acc', acc, on_step=False, on_epoch=True)
         self.log('val_precision', precision, on_step=False, on_epoch=True)
         self.log('val_recall', recall, on_step=False, on_epoch=True)
+        self.log('val_auroc', auroc, on_step=False, on_epoch=True)
 
         return loss
 
 if __name__ == "__main__":
+
+    # ds = IncidentDataset()
+    # a = sum(ds[:][1])
+    # b = len(ds[:][1])
+    # print(b/a)
     idm = IncidentDataModule()
     idm.setup()
+    tdt = idm.train_dataloader()
+    features, labels = next(iter(tdt))
+    print(features.shape)
 
     incident_model = IncidentModel()
     logger = TensorBoardLogger('./logs', name = 'incident-model')
-    trainer = pl.Trainer(logger = logger, max_epochs = 10)
+    trainer = pl.Trainer(logger = logger, max_epochs = 20, accelerator='gpu')
     trainer.fit(incident_model, idm)
