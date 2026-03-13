08c8e7458a901116d25557eead5a85ea47389c01
adimac13 <s197932@student.pg.edu.pl>
2026-03-13 21:48:15 +0100

    updated lstm model

--- lstm_model.py
+++ lstm_model.py
@@ -7,8 +7,8 @@ import torch
 import torchmetrics
 
 class IncidentDataset(Dataset):
-    def __init__(self, current_window_size = 200, future_window_size = 100, freq = 2, time_range = 20, num_of_probes = 10000):
-        sg = SignalGenerator(future_window_size, current_window_size, freq, time_range, num_of_probes)
+    def __init__(self, current_window_size = 200, future_window_size = 100, freq = 2, time_range = 20, num_of_probes = 10000, seed = 0):
+        sg = SignalGenerator(future_window_size, current_window_size, freq, time_range, num_of_probes, seed)
         self.input, self.labels = sg.prepare_dataset()
 
     def __len__(self):
@@ -23,11 +23,20 @@ class IncidentDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
 
     def setup(self, stage = None):
-        ds = IncidentDataset()
-        self.ds = ds
-        train_ds_len = len(ds) - len(ds) // 10
-        self.train_dataset = Subset(ds, range(0,train_ds_len))
-        self.val_dataset = Subset(ds, range(train_ds_len,len(ds)))
+        # ds = IncidentDataset()
+        #
+        # labels = torch.tensor(ds[:][1])
+        # pos = labels.sum()
+        # neg = len(labels) - pos
+        # pos_weight = neg / pos
+        # print(f"pos_weight: {pos_weight:.2f}")
+        #
+        # self.ds = ds
+        # train_ds_len = len(ds) - len(ds) // 10
+        # self.train_dataset = Subset(ds, range(0,train_ds_len))
+        # self.val_dataset = Subset(ds, range(train_ds_len,len(ds)))
+        self.train_dataset = IncidentDataset(seed = 0)
+        self.val_dataset = IncidentDataset(seed = 42)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size = self.batch_size, shuffle = True)
@@ -38,12 +47,9 @@ class IncidentDataModule(pl.LightningDataModule):
 class IncidentModel(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(input_size = 1, hidden_size=128, num_layers=2, batch_first = True, dropout = 0.2)
-        self.fc1 = nn.Linear(128, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 1)
-        self.relu  = nn.ReLU()
-        self.loss = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(2.0))
+        self.lstm = nn.LSTM(input_size=1, hidden_size=16, num_layers=1, batch_first=True)
+        self.fc1 = nn.Linear(16, 1)
+        self.loss = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(5.0))
 
         self.acc_train = torchmetrics.Accuracy(task='binary')
         self.precision_train  = torchmetrics.Precision(task='binary')
@@ -62,15 +68,17 @@ class IncidentModel(pl.LightningModule):
         lstm_out, _ = self.lstm(x)
         lstm_final = lstm_out[:,-1,:]
         out = self.fc1(lstm_final)
-        out = self.relu(out)
-        out = self.fc2(out)
-        out = self.relu(out)
-        out = self.fc3(out)
         return out
 
     def configure_optimizers(self):
-        optimizer = optim.AdamW(self.parameters(), lr = 1e-4)
-        return optimizer
+        optimizer = optim.AdamW(self.parameters(), lr=3e-5)  # było 1e-4
+        scheduler = optim.lr_scheduler.OneCycleLR(
+            optimizer,
+            max_lr=3e-4,
+            total_steps=self.trainer.estimated_stepping_batches  # bez dzielenia
+        )
+        return {"optimizer": optimizer,
+                "lr_scheduler": {"scheduler": scheduler, "interval": "step"}}
 
     def training_step(self, batch):
         inputs, labels = batch
@@ -109,18 +117,10 @@ class IncidentModel(pl.LightningModule):
         return loss
 
 if __name__ == "__main__":
-
-    # ds = IncidentDataset()
-    # a = sum(ds[:][1])
-    # b = len(ds[:][1])
-    # print(b/a)
     idm = IncidentDataModule()
     idm.setup()
-    tdt = idm.train_dataloader()
-    features, labels = next(iter(tdt))
-    print(features.shape)
 
     incident_model = IncidentModel()
     logger = TensorBoardLogger('./logs', name = 'incident-model')
-    trainer = pl.Trainer(logger = logger, max_epochs = 20, accelerator='gpu')
+    trainer = pl.Trainer(logger = logger, max_epochs = 50, accelerator='gpu', gradient_clip_val=1.0)
     trainer.fit(incident_model, idm)
