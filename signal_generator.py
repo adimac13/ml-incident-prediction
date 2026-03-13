e67310f3dd4e7c025288fb7fe01f3708b804eeef
adimac13 <s197932@student.pg.edu.pl>
2026-03-13 21:48:33 +0100

    improved data labelling

--- signal_generator.py
+++ signal_generator.py
@@ -6,7 +6,7 @@ class SignalGenerator:
     This class prepares a dataset for training. It generates a linearly increasing sine function with noise and incident
     applied.
     """
-    def __init__(self, next_H_steps = 20, prev_W_steps = 20, freq = 2, time_range = 20, num_of_probes = 10000):
+    def __init__(self, next_H_steps = 100, prev_W_steps = 200, freq = 2, time_range = 20, num_of_probes = 10000, seed = 0):
         self.freq = freq
         self.time_range = time_range
         self.num_of_probes = num_of_probes
@@ -14,41 +14,65 @@ class SignalGenerator:
         self.signal = None
         self.H_steps = next_H_steps
         self.W_steps = prev_W_steps
+        self.seed = seed
 
-    def setup(self):
+    def _setup(self):
         """
         Setup function. Function creates signal and labels, where: 0 -> normal, 1 -> incident.
         """
+        np.random.seed(self.seed)
         time = np.linspace(0, self.time_range, self.num_of_probes)
-        noise = np.random.normal(0, 0.1, self.num_of_probes) / 10
-        sine = np.sin(time * self.freq) / 10
-        linear = time / 10
+        noise = np.random.normal(0, 0.1, self.num_of_probes) / 20
+        sine = np.sin(time * self.freq) / 20
+        linear = 0
 
         self.signal = noise + sine + linear
-        i = 0
-        while i < self.num_of_probes - 1:
-            gap = int(np.random.exponential()*700)
-            i += gap
-            len_of_incident = np.random.randint(50, 100)
-            self.signal[i : (i+len_of_incident)] += 0.2
-            self.where_incident[i : (i+len_of_incident)] = 1
 
-            i += len_of_incident # To avoid incident overlapping
+        pre_incident_len = self.W_steps
+        linspace = np.linspace(-2,0,pre_incident_len)
+        pre_incident = np.exp(linspace)
+
+
+        i = pre_incident_len
+        while True:
+            gap = int(np.random.exponential()*300)
+            i += gap
+            if i > len(self.signal) - 1:
+                break
+
+            len_of_incident = np.random.randint(100, 200)
+
+            self.signal[i : (i+len_of_incident)] += 0.2
+            self.signal[i - pre_incident_len : i] += pre_incident * 0.2
+            self.where_incident[i: (i+len_of_incident)] = 1
+
+            i += len_of_incident + 800 # To avoid incident overlapping
+
+        self.signal = (self.signal - min(self.signal)) / (max(self.signal) - min(self.signal))
+
+
+        # plt.plot(time, self.signal)
+        # plt.fill_between(time, plt.ylim()[0], plt.ylim()[1], where=self.where_incident == 1, color='red', alpha=0.3)
+        # plt.show()
 
     def prepare_dataset(self):
         """
         Function returns windows (consisting of W steps) and label whether in next H steps incident will occur.
         """
-        self.setup()
+        self._setup()
         xs = []
         ys = []
-        for i in range(len(self.signal) - self.W_steps - 1):
+        for i in range(0, len(self.signal) - self.W_steps - 1, 5):
+            # Getting rid of samples containing incident
+            if 1 in self.where_incident[i:i + self.W_steps]:
+                continue
+
             # W step window
             x = self.signal[i:i + self.W_steps]
 
-            # H step window, after W step window
+            # H steps window, after W steps window
             max_y_idx = min(len(self.signal) - 1, i + self.W_steps + self.H_steps)
-            y = (1 if 1 in self.where_incident[i + self.W_steps : max_y_idx + 1] else 0)
+            y = (1 if 1 in self.where_incident[i + self.W_steps : max_y_idx ] else 0)
 
             xs.append(x)
             ys.append(y)
@@ -57,6 +81,7 @@ class SignalGenerator:
         ys = np.array(ys)
 
         trainX = xs[:,:,np.newaxis]
+
         trainY = ys[:, np.newaxis]
 
         return trainX, trainY
@@ -64,5 +89,6 @@ class SignalGenerator:
 if __name__ == "__main__":
     ds = SignalGenerator()
     x,y = ds.prepare_dataset()
-    print(len(x))
+    print(y.mean())
+    # ds._setup()
     # pass
