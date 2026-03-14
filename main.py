9788a12136af197b59a67b135c68b59c14a605b4
adimac13 <s197932@student.pg.edu.pl>
2026-03-14 19:39:01 +0100

    training for gbc and lstm

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
-    pass
+from lstm_model import IncidentModel_train
+from gbc_model import IncidentModel as IncidentModel_gbc
 
 if __name__ == "__main__":
-    main()
+    # To train lstm model
+    # IncidentModel_train()
+
+    # To train and evaluate gbc model
+    im = IncidentModel_gbc()
+    im.train()
+    im.test()
